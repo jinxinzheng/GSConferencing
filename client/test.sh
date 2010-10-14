a993b7eaf8a8b8d2421845196c6e45ce194bceb5
Jinxin Zheng <jinxinzheng@gmail.com>
2010-10-14 12:42:02 +0800

    test client: testing clients unordered.

--- client/test.sh
+++ client/test.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-./test-client -i 801 &
-./test-client -i 901 &
+./test-client -i 101 &
+./test-client -i 102 -s 9 &
+./test-client -i 103 -s 8 &
 
 sleep 2
 
-./test-client -i 101 &
-./test-client -i 102 &
-./test-client -i 103 -s 8 &
+./test-client -i 801 &
+./test-client -i 901 &

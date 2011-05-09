681a4f94724fae9b0e61b21fb46089428b9a1e3a
Jinxin Zheng <jinxinzheng@gmail.com>
2011-05-09 12:46:21 +0800

    ptc: go to default position when all devs are removed.
    the default position of a ptc dev is stored in its ptc_cmd.

--- serv/ptc.c
+++ serv/ptc.c
@@ -8,6 +8,40 @@
 #include "db/md.h"
 #include <stdio.h>
 
+static void ptc_send(struct device *d);
+
+/* all the ptc devs that we have ever known. */
+static struct device *ptcs[32] = {NULL};
+
+static inline void add_ptc(struct device *ptc)
+{
+  int i;
+
+  for( i=0 ; i<sizeof(ptcs)/sizeof(ptcs[0]) ; ++i )
+  {
+    if( ptc == ptcs[i] )
+      break;
+
+    if( !ptcs[i] )
+    {
+      ptcs[i] = ptc;
+      break;
+    }
+  }
+}
+
+static inline void ptc_default()
+{
+  int i;
+  for( i=0 ; i<sizeof(ptcs)/sizeof(ptcs[0]) ; ++i )
+  {
+    if( !ptcs[i] )
+      break;
+
+    ptc_send(ptcs[i]);
+  }
+}
+
 /* send the cmd to the 'ptc' device,
  * not the device itself. */
 static void ptc_send(struct device *d)
@@ -24,6 +58,8 @@ static void ptc_send(struct device *d)
       l = sprintf(buf, "%d ptc %s\n", (int)d->id, ptcmd);
       sendto_dev_tcp(buf, l, ptc);
     }
+
+    add_ptc(ptc);
   }
 }
 
@@ -50,18 +86,22 @@ void ptc_remove(struct device *d)
 
   list_del(&d->ptc.l);
 
+  if( list_empty(&ptc_head) )
+  {
+    /* if all are removed, the ptc should
+     * go to the default position. */
+    ptc_default();
+
+    curr = NULL;
+
+    return;
+  }
+
   if( d == curr )
   {
     /* if the lastly pushed dev is removed,
      * the ptc should go to the prev dev. */
-    if( !list_empty(&ptc_head) )
-    {
-      curr = list_entry(ptc_head.next, struct device, ptc.l);
-      ptc_send(curr);
-    }
-    else
-    {
-      curr = NULL;
-    }
+    curr = list_entry(ptc_head.next, struct device, ptc.l);
+    ptc_send(curr);
   }
 }

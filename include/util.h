9c32d6f0c65b99ef41821f7d04d50c0a799d7a99
Jinxin Zheng <jinxinzheng@gmail.com>
2011-11-28 13:43:53 +0800

    use ssize_t for the offsetof macro.
    ssize_t is better than size_t as we mostly use signed int to represent
    length.

--- include/util.h
+++ include/util.h
@@ -6,7 +6,7 @@
 #define die(s) do {perror(s); exit(1);} while(0)
 #define fail(s) do {perror(s); return -1;} while(0)
 
-#define offsetof(type, memb)  (size_t)&(((type *)0)->memb)
+#define offsetof(type, memb)  (ssize_t)&(((type *)0)->memb)
 
 #define array_size(arr)   ((int)(sizeof(arr) / sizeof((arr)[0])))
 
